f861a0dab42b809b828faf5630bc1d44143c6fbe
david <I504105@mvrht.com>
2017-03-02 19:54:05 +0100

    Added new fillable areas

--- laravel/app/Product.php
+++ laravel/app/Product.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'price',
+        'shortDescription',
+        'longDescription',
+        'images'
+    ];
 }
