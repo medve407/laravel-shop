c73e983c3de54c3b321d8d36e823ebf3b6df66cd
david <I504105@mvrht.com>
2017-03-02 21:08:18 +0100

    Modified the routes: - added login - logout - signup

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,6 +11,43 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+ * Return the index page.
+ */
+Route::get('/', function () { return view('index'); });
+
+//////////////////////////
+//                      //
+//     PRODUCT PART     //
+//                      //
+//////////////////////////
+/**
+ * Return the products layout.
+ */
+Route::get('/products',"ProductController@getProducts");
+
+//////////////////////////
+//                      //
+//     USER   PART      //
+//                      //
+//////////////////////////
+/**
+ * It redirect to login page
+ */
+Route::get('/login',"UserController@getLogin");
+/*
+ * Use for send the form data
+ */
+Route::post('/login',"UserController@postLogin");
+/**
+ * It logout the user.
+ */
+Route::get('/logout',"UserController@getLogout");
+/*
+ * It redirect to Sign in page
+ */
+Route::get('/signup',"UserController@getSignup");
+/*
+ * Use for send the form data
+ */
+Route::post('/signup',"UserController@postSignup");
