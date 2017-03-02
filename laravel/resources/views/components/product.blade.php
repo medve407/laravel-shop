8ac76bcfbf8b69e1167ca29be7b6639d1d60e46b
david <I504105@mvrht.com>
2017-03-02 19:54:25 +0100

    Sent the product details for product.blade layout

--- laravel/resources/views/components/product.blade.php
+++ laravel/resources/views/components/product.blade.php
@@ -1,10 +1,10 @@
 <div class="panel panel-default">
-    <div class="panel-heading">Product title</div>
+    <div class="panel-heading">{{ $product->title }}</div>
     <div class="panel-body">
         <p>Image</p>
-        <p>Product short description</p>
-        <p>Product long description</p>
-        <p>Price</p>
-        <p><a href="#" class="btn btn-primary" role="button">To cart</a> <a href="#" class="btn btn-default" role="button">Details</a></p>
+        <p>{{ $product->shortDescription }}</p>
+        <p>{{ $product->longDescription }}</p>
+        <p>{{ $product->price }}</p>
+        <p><a href="#" class="btn btn-primary" role="button">To cart</a> <a href="{{ url('product/'. $product->id) }}" class="btn btn-default" role="button">Details</a></p>
     </div>
 </div>
