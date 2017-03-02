6fdd10f7475ded75a15fea9e8710a30a02b06f53
david <I504105@mvrht.com>
2017-03-02 22:47:05 +0100

    Added a listener for product exists or not, if not it says product is empty

--- laravel/resources/views/components/product.blade.php
+++ laravel/resources/views/components/product.blade.php
@@ -1,10 +1,14 @@
-<div class="panel panel-default">
-    <div class="panel-heading">{{ $product->title }}</div>
-    <div class="panel-body">
-        <p>Image</p>
-        <p>{{ $product->shortDescription }}</p>
-        <p>{{ $product->longDescription }}</p>
-        <p>{{ $product->price }}</p>
-        <p><a href="#" class="btn btn-primary" role="button">To cart</a> <a href="{{ url('product/'. $product->id) }}" class="btn btn-default" role="button">Details</a></p>
+@if(isset($product))
+    <div class="panel panel-default">
+        <div class="panel-heading">{{ $product->title }}</div>
+        <div class="panel-body">
+            <p>Image</p>
+            <p>{{ $product->shortDescription }}</p>
+            <p>{{ $product->longDescription }}</p>
+            <p>{{ $product->price }}</p>
+            <p><a href="#" product-id="{{ $product->id }}"class="to-cart btn btn-primary" role="button">To cart</a> <a href="{{ url('product/'. $product->id) }}" class="btn btn-default" role="button">Details</a></p>
+        </div>
     </div>
-</div>
+@else
+    <h1 class="text-center">Sorry, but this product is not exists :(</h1>
+@endif
