4eb3605819332793b263f1504bbf580631ec0248
david <I504105@mvrht.com>
2017-03-02 22:47:53 +0100

    Added jquery and a script which show that the function is clicked.

--- laravel/resources/views/layouts/main.blade.php
+++ laravel/resources/views/layouts/main.blade.php
@@ -2,9 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <!-- Csrf token -->
+    <meta name="_token" content="{{ csrf_token() }}">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Optional theme -->
@@ -20,12 +24,14 @@
     @include('layouts.navigation')
     <div class="container">
         @if(!empty( session('message') ))
-            <div class="alert alert-info fade out">
+            <div class="alert alert-info fade in">
                 <p> {{  session('message') }}</p>
             </div>
         @endif
+
         <!-- Main content -->
         @yield('content')
     </div>
 </body>
+<script src="{{ URL::to('js/shop.js') }}"></script>
 </html>
