bf1677f7b1f611db0f6e7c1e8cf34cde9a5f3785
david <I504105@mvrht.com>
2017-03-02 19:06:03 +0100

    Basic layout for one product

---
+++ laravel/resources/views/components/product.blade.php
@@ -0,0 +1,10 @@
+<div class="panel panel-default">
+    <div class="panel-heading">Product title</div>
+    <div class="panel-body">
+        <p>Image</p>
+        <p>Product short description</p>
+        <p>Product long description</p>
+        <p>Price</p>
+        <p><a href="#" class="btn btn-primary" role="button">To cart</a> <a href="#" class="btn btn-default" role="button">Details</a></p>
+    </div>
+</div>
