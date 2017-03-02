be9a4df0ce6ad40a3cf4a8a8bac1c7d27cbdefed
david <I504105@mvrht.com>
2017-03-02 22:47:28 +0100

    modified the class name

--- laravel/resources/views/layouts/navigation.blade.php
+++ laravel/resources/views/layouts/navigation.blade.php
@@ -10,12 +10,13 @@
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="{{ url('/') }}">Home</a></li>
+                <li><a href="{{ url('/') }}">Home</a></li>
                 <li><a href="{{ url('/products') }}">Products</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 @if(Auth::check())
-                    <li><a href="{{ url('/logout') }}"><span class="glyphicon glyphicon-log-in"></span> Log out</a></li>                <li><a href="{{ url('/cart') }}"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+                    <li><a href="{{ url('/logout') }}"><span class="glyphicon glyphicon-log-in"></span> Log out</a></li>
+                    <li><a href="{{ url('/cart') }}"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
                 @else
                     <li><a href="{{ url('/login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                     <li><a href="{{ url('/signup') }}"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
