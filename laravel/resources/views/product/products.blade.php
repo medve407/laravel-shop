4ddad5fe235330f55157190a00b9160044249c94
david <I504105@mvrht.com>
2017-03-02 19:53:55 +0100

    It now show the products

--- laravel/resources/views/product/products.blade.php
+++ laravel/resources/views/product/products.blade.php
@@ -2,4 +2,8 @@
 
 @section('content')
     <h1>Products</h1>
+    <!-- Products -->
+    @foreach($products as $product)
+        @include('components.product', ['product' => $product])
+    @endforeach
 @endsection
